1dee9bee74d546119d67417fe4d0e5e55b1803c4
Nika <129855721+Nika-cyber1@users.noreply.github.com>
2023-09-15 10:04:25 +0300

    Update portfolio.wjst.js

--- portfolio.wjst.js
+++ portfolio.wjst.js
@@ -2,7 +2,7 @@ import Crud from '/api/wjst/crud';
 class Service extends Crud {
 	getName = 'public';
 	constructor() {
-		super('/api/service');
+		super('/api/portfolio');
 	}
 }
 export default new Service();
