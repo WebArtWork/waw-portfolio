e338856ff043f4a0df326e0f5b0d2b7f47751ba4
Honchar Denys <crackeraki@gmail.com>
2023-10-01 23:19:24 +0300

    fixes issues with pretify

--- portdolio.api.js
+++ portdolio.api.js
@@ -1,15 +1,9 @@
-const path = require('path');
-const template = path.join(process.cwd(), 'template');
+const path = require("path");
+const template = path.join(process.cwd(), "template");
 
-module.exports = async waw => {
-	const seo = {
-		title: waw.config.name,
-		description: waw.config.description,
-		image: 'https://body.webart.work/template/img/logo.png'
-	};
-
-	waw.build(template, 'portfolios');
-	waw.build(template, 'portfolio');
+module.exports = async (waw) => {
+	waw.build(template, "portfolios");
+	waw.build(template, "portfolio");
 	waw.serve_portfolios = {};
 	waw.serve_portfolio = {};
 	const portfolios = async (req, res) => {
@@ -17,53 +11,54 @@ module.exports = async waw => {
 			waw.serve_portfolios[req.get("host")](req, res);
 		} else {
 			const portfolios = await waw.portfolios(
-				req.params.tag_id ?
-					{ tag: req.params.tag_id } :
-					{}
+				req.params.tag_id ? { tag: req.params.tag_id } : {}
 			);
 
 			res.send(
 				waw.render(
-					path.join(template, 'dist', 'portfolios.html'),
+					path.join(template, "dist", "portfolios.html"),
 					{
-						...seo,
-						title: waw.config.portfolioTitle|| waw.config.title,
-                                                description: waw.config.portfolioDescription || waw.config.description,
-                                                image: waw.config.portfolioImage|| waw.config.image,
+						...waw.config,
+						title: waw.config.portfolioTitle || waw.config.title,
+						description:
+							waw.config.portfolioDescription ||
+							waw.config.description,
+						image: waw.config.portfolioImage || waw.config.image,
 						portfolios,
-						categories: await waw.tag_groups('portfolio')
+						categories: await waw.tag_groups("portfolio"),
 					},
 					waw.translate(req)
 				)
-			)
+			);
 		}
 	};
-	waw.app.get('/portfolios', portfolios);
-	waw.app.get('/portfolios/:tag_id', portfolios);
-	waw.app.get('/portfolio/:_id', async (req, res) => {
+	waw.app.get("/portfolios", portfolios);
+	waw.app.get("/portfolios/:tag_id", portfolios);
+	waw.app.get("/portfolio/:_id", async (req, res) => {
 		if (typeof waw.serve_portfolios[req.get("host")] === "function") {
 			waw.serve_portfolios[req.get("host")](req, res);
 		} else {
-
-			const portfolio = await Portflio.findOne({
-				_id: req.params._id
+			const portfolio = await waw.Portflio.findOne({
+				_id: req.params._id,
 			});
 
 			res.send(
 				waw.render(
-					path.join(template, 'dist', 'portfolio.html'),
+					path.join(template, "dist", "portfolio.html"),
 					{
-						...seo,
+						...waw.config,
 						portfolio
 					},
 					waw.translate(req)
 				)
-			)
+			);
 		}
 	});
 
 	waw.portfolios = async (query = {}, limit, count = false) => {
-		let exe = count ? waw.Portfolio.countDocuments(query) : waw.Portfolio.find(query);
+		let exe = count
+			? waw.Portfolio.countDocuments(query)
+			: waw.Portfolio.find(query);
 		if (limit) {
 			exe = exe.limit(limit);
 		}
@@ -72,7 +67,7 @@ module.exports = async waw => {
 
 	waw.portfolio = async (query) => {
 		return await waw.Portfolio.findOne(query);
-	}
+	};
 
 	const save_file = (doc) => {
 		if (doc.thumb) {
@@ -84,11 +79,11 @@ module.exports = async waw => {
 				waw.save_file(thumb);
 			}
 		}
-	}
+	};
 
-	waw.on('portfolio_create', save_file);
-	waw.on('portfolio_update', save_file);
-	waw.on('portfolio_delete', (doc) => {
+	waw.on("portfolio_create", save_file);
+	waw.on("portfolio_update", save_file);
+	waw.on("portfolio_delete", (doc) => {
 		if (doc.thumb) {
 			waw.delete_file(doc.thumb);
 		}
@@ -100,9 +95,9 @@ module.exports = async waw => {
 		}
 	});
 	await waw.wait(2000);
-          if (waw.store_landing) {
-          waw.store_landing.portfolios = async (query)=>{
-         return await waw.portfolios(query, 4);
-          }
-       }
+	if (waw.store_landing) {
+		waw.store_landing.portfolios = async (query) => {
+			return await waw.portfolios(query, 4);
+		};
+	}
 };
